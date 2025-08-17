bbd67c2b431717c81d8ea72625dd9de09f284772
Abdul-1421 <abdulrahman.alodaib2@gmail.com>
2025-08-17 19:19:12 +0300

    woek

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(0, 180, 0);
+    }
+}
