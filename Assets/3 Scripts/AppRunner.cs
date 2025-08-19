5de1e13c831384cdfd12e044a725ddf5d8d6289c
d7meDev <d7me446@gmail.com>
2025-08-19 20:52:31 +0300

    rollback

--- Assets/3 Scripts/AppRunner.cs
+++ Assets/3 Scripts/AppRunner.cs
@@ -1,74 +1,31 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class AppRunner : MonoBehaviour
 {
     public GameObject viewApp;
     public GameObject controls;
-    public GameObject Background;
     public string currentApp;
-    public static int Counter = 0;
-    public GameObject apps;
+    public int Counter = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
- 
-
+    
     public void runApp(string appName)
     {
-        Counter++;
-        bool isLoading = Counter % 2 != 0;
-
-        if (!isLoading)
-        {
-            RawImage rawImg = Background.GetComponent<RawImage>();
-            Texture2D tex = Resources.Load<Texture2D>("loading");
-
-            viewApp.SetActive(true);
-            controls.SetActive(true);
-            apps.SetActive(true);
-
-             rawImg = viewApp.GetComponent<RawImage>();
-
-             tex = Resources.Load<Texture2D>(appName);
-
-            rawImg.texture = tex;
-
-            Transform child = transform.Find("controls").transform.Find(appName.Split("_")[0] + "_controls");
-            child.gameObject.SetActive(true);
-            this.currentApp = appName.Split("_")[0];
-            Debug.Log(appName);
-
-            rawImg.enabled = true;
-
-            isLoading = !isLoading;
-            
-        }
-
-        else
-        {
-            viewApp.SetActive(false);
-            controls.SetActive(false);
-            apps.SetActive(false);
-
-            RawImage rawImg = Background.GetComponent<RawImage>();
-            Texture2D tex = Resources.Load<Texture2D>("loading");
-            rawImg.texture = tex;
 
 
-        }
 
-        //RawImage rawImg = viewApp.GetComponent<RawImage>();
+        RawImage rawImg = viewApp.GetComponent<RawImage>();
 
-        //Texture2D tex = Resources.Load<Texture2D>(appName);
+        Texture2D tex = Resources.Load<Texture2D>(appName);
 
-        //rawImg.texture = tex;
+        rawImg.texture = tex;
 
-        //Transform child = transform.Find("controls").transform.Find(appName.Split("_")[0] + "_controls");
-        //child.gameObject.SetActive(true);
-        //this.currentApp = appName.Split("_")[0];
-        //Debug.Log(appName);
+        Transform child = transform.Find("controls").transform.Find(appName.Split("_")[0] + "_controls");
+        child.gameObject.SetActive(true);
+        this.currentApp = appName.Split("_")[0];
+        Debug.Log(appName);
 
-        //rawImg.enabled = true;
+        rawImg.enabled = true;
 
     }
 
