5ccdbd32667bbe1520325814a373568d221a054e
mqo5 <meshalq2005@gmail.com>
2025-08-20 16:49:22 +0300

    UI done and Panel changes

--- Assets/3 Scripts/UI Scripts/GuidePanelScript.cs
+++ Assets/3 Scripts/UI Scripts/GuidePanelScript.cs
@@ -15,35 +15,39 @@ public class GuidePanelScript : MonoBehaviour
     {
         if (AppRunner.Counter == 0)
         {
-            text.text = "Welcome to the Guide Panel!";
+            text.text = "In the Desktop Open Discord App";
         }
         if (AppRunner.Counter == 1)
         {
-            text.text = "1!";
+            text.text = "In the motherboard Move Data from storage, to RAM, lastly to the CPU for the discord app to work.";
         }
         if (AppRunner.Counter == 2)
         {
-            text.text = "2!";
+            text.text = "In the Desktop In the Discord App, click the URL";
         }
         if (AppRunner.Counter == 3)
         {
-            text.text = "3!";
+            text.text = "In the motherboard Move Data from storage, to RAM, lastly to the CPU for the Google chrome app to work.";
         }
         if (AppRunner.Counter == 4)
         {
-            text.text = "4!";
+            text.text = "In the Desktop, click the download button.";
         }
         if (AppRunner.Counter == 5)
         {
-            text.text = "5!";
+            text.text = "In the Motherboard, Move Data from the Network Adapter, to the Storage to save the downloaded file.";
         }
-        if (AppRunner.Counter== 6)
+        if (AppRunner.Counter == 6)
         {
-            text.text = "6!";
+            text.text = "In the Desktop, open the downloaded file.";
         }
         if (AppRunner.Counter == 7)
         {
-            text.text = "7!";
+            text.text = "In the motherboard Move Data from storage, to RAM, lastly to the CPU for the downloaded app to work.";
+        }
+                if (AppRunner.Counter == 8)
+        {
+            text.text = "HACKED, if there is no backup";
         }
         // This script currently does not have any functionality.
         // It can be used to manage the guide panel UI or other related tasks in the future.
