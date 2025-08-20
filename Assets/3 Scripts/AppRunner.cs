e519e7c94c46a839c7767b74d7c8d93f92912cf0
d7meDev <d7me446@gmail.com>
2025-08-20 05:35:48 +0300

    AppRunner modifications

--- Assets/3 Scripts/AppRunner.cs
+++ Assets/3 Scripts/AppRunner.cs
@@ -5,35 +5,102 @@ public class AppRunner : MonoBehaviour
 {
     public GameObject viewApp;
     public GameObject controls;
-    public string currentApp;
-    public int Counter = 0;
+    public GameObject Background;
+    public GameObject apps;
+    public GameObject close;
+    public static string currentApp;
+    public static int Counter = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
+    public void increaseCount()
+    {
+       
+        runApp(currentApp); 
+    }
+
+    
+
     public void runApp(string appName)
     {
 
 
 
-        RawImage rawImg = viewApp.GetComponent<RawImage>();
+        Counter++;
+        if (!string.IsNullOrEmpty(appName))
+        {
+            currentApp = appName;
+        }
+        currentApp = appName;
 
-        Texture2D tex = Resources.Load<Texture2D>(appName);
+        if (Counter == 0)
+        {
+            
+            return;
+        }
 
-        rawImg.texture = tex;
+        else if (Counter % 2 == 0)
+        {
+            
+            Debug.Log(currentApp);
+            controls.SetActive(true);
 
-        Transform child = transform.Find("controls").transform.Find(appName.Split("_")[0] + "_controls");
-        child.gameObject.SetActive(true);
-        this.currentApp = appName.Split("_")[0];
-        Debug.Log(appName);
+            RawImage rawImg = viewApp.GetComponent<RawImage>();
 
-        rawImg.enabled = true;
+            Texture2D tex = Resources.Load<Texture2D>(appName);
+
+            rawImg.texture = tex;
+
+            Transform child = transform.Find("controls").transform.Find(appName.Split("_")[0] + "_controls");
+            child.gameObject.SetActive(true);
+            
+            close.SetActive(true);
+
+
+
+
+            rawImg.enabled = true;
+
+        }
+
+        else {
+
+            // viewApp.SetActive(false);
+            controls.SetActive(false);
+            apps.SetActive(false);
+            close.SetActive(false);
+            Debug.Log("lost");
+
+
+
+            RawImage rawImg = Background.GetComponent<RawImage>();
+            Texture2D tex = Resources.Load<Texture2D>("loading");
+
+            rawImg.texture = tex;
+
+
+             rawImg = viewApp.GetComponent<RawImage>();
+             rawImg.enabled = false;
+             
+
+        }
 
     }
 
     public void closeApp()
     {
+        Counter++;
         RawImage rawImg = viewApp.GetComponent<RawImage>();
         rawImg.enabled = false;
-        Transform child = transform.Find("controls").transform.Find(currentApp + "_controls");
+        Transform child = transform.Find("controls").transform.Find(currentApp.Split("_")[0] + "_controls");
         child.gameObject.SetActive(false);
+        currentApp = null;
+
+         rawImg = Background.GetComponent<RawImage>();
+        Texture2D tex = Resources.Load<Texture2D>("bg");
+
+        rawImg.texture = tex;
+
+        apps.SetActive(true);
+        
     }
 }
